3817df9eaa967dc09386dd30a601f4d0c76603da
Takashi UNO <a99s504@auone.jp>
2011-05-05 15:55:27 +0900

    add tableView and Twitter timeline.

--- Resources/app.js
+++ Resources/app.js
@@ -18,24 +18,44 @@ var tab1 = Titanium.UI.createTab({
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'Hello World',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var data = [];
+var tableView = Ti.UI.createTableView({
+	data:data
 });
 
-win1.add(label1);
+function updateTimeline (timeline) {
+    var currentData = [];
+    for (var i=0;i<timeline.length;i++) {
+	var tweet = timeline[i];
+	var row = Ti.UI.createTableViewRow();
+	var commentLabel = Ti.UI.createLabel();
+	commentLabel.text = tweet.text;
+	row.add(commentLabel);
+	currentData.push(row);
+    }
+    tableView.setData(currentData);
+}
+
+var xhr = Ti.Network.createHTTPClient();
+var user = 'richard_raw';
+var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + user;
+xhr.open('GET', url);
+xhr.onload = function() {
+    var timeline = JSON.parse(this.responseText);
+    updateTimeline(timeline);
+};
+xhr.send();
+
+win1.add(tableView);
 
 //
 // create controls tab and root window
 //
-var win2 = Titanium.UI.createWindow({  
+var win2 = Titanium.UI.createWindow({
     title:'Tab 2',
     backgroundColor:'#fff'
 });
-var tab2 = Titanium.UI.createTab({  
+var tab2 = Titanium.UI.createTab({
     icon:'KS_nav_ui.png',
     title:'Tab 2',
     window:win2
@@ -57,7 +77,7 @@ win2.add(label2);
 //  add tabs
 //
 tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+tabGroup.addTab(tab2);
 
 
 // open tab group
